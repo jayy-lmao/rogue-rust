06ad91c50477c6cfdc815d9a744a6436ee2a3d58
James Holman <jameswarringtonholman@gmail.com>
2020-09-18 15:42:18 +1000

    feat: started on rust roguelike tutorial

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::image::{InitFlag, LoadTexture};
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::{Texture, WindowCanvas};
+use std::time::Duration;
+
+pub fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect,
+) -> Result<(),String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.copy(&texture,Rect::new(0,0,26,36),Rect::new(0,0,26,36))?;
+    canvas.present();
+    Ok(())
+}
+
+pub fn main() -> Result<(),String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+    let window = video_subsystem
+        .window("Game tutorial", 800, 600)
+        .position_centered()
+        .build()
+        .expect("Could not init video");
+
+    let mut canvas = window.into_canvas().build().expect("Could not convert window to canvas");
+
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let position = Point::new(0,0);
+    let sprite = Rect::new(0,0,26,26);
+
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut i = 0;
+    'running: loop {
+        // Get Input
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(
+            &mut canvas,
+            Color::RGB(i, 64, 255 - i),
+            &texture,
+            position,
+            sprite,
+        )?;
+
+        // The rest of the game loop goes here...
+
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+    Ok(())
+}
